43d02ba5892afed2b1fef5298a8c8caa7ca931e9
Nikoloz <nikalatsabidze@gmail.com>
2023-06-20 13:31:55 +0400

    INIT

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { AppService } from './app.service';
+import { S3Service } from './s3.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,private s3Service: S3Service) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    const response = await this.s3Service.uploadFile(file);
+    return response;
+  }
+}
---
+++ src/s3.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@nestjs/common";
+import * as AWS from "aws-sdk";
+import { config } from "dotenv";
+
+config();
+
+@Injectable()
+export class S3Service {
+  AWS_S3_BUCKET = process.env.AWS_S3_BUCKET_NAME;
+  s3 = new AWS.S3
+  ({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  });
+
+  async uploadFile(file) {
+    const { originalname } = file;
+    await this.s3_upload(file.buffer, this.AWS_S3_BUCKET, originalname, file.mimetype);
+  }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params =
+      {
+        Bucket: bucket,
+        Key: String(name),
+        Body: file,
+
+        ContentType: mimetype,
+        ContentDisposition: "inline"
+
+      };
+
+    console.log(params);
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      console.log(s3Response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
